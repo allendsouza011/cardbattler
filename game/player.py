316ad609a35c72ccfbf6d5640139d4cd2239b181
allendsouza011 <allendsouza011@gmail.com>
2024-05-26 21:49:05 +0000

    changes

--- game/player.py
+++ game/player.py
@@ -1,16 +1,16 @@
 from game.card import Card
-import random
 
 class Player:
     def __init__(self, name, health):
         self.name = name
         self.health = health
-        self.deck = []
+        self.deck = None
         self.hand = []
 
     def draw_card(self):
-        if self.deck:
-            self.hand.append(self.deck.pop())
+        card = self.deck.draw_card()
+        if card:
+            self.hand.append(card)
 
     def draw_initial_hand(self, count=5):
         for _ in range(count):
